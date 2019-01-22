e2067b422acfb64bc7ef14cd3bef5cf31dfaf728
박현진 <bestcure9@gmail.com>
2019-01-22 15:30:35 +0900

    aws properties

--- src/main/java/com/test/s3/SpringS3UploadApplication.java
+++ src/main/java/com/test/s3/SpringS3UploadApplication.java
@@ -2,8 +2,10 @@ package com.test.s3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:${aws.config}" })
 public class SpringS3UploadApplication {
 
 	public static void main(String[] args) {
